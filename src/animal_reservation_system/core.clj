516050b13a7a1cfbee2f499f1bd071843f85dc0a
Alex Rodioukov <alex@gurudom.com>
2016-05-10 18:26:17 -0600

    N horses - pick n animals at random

--- src/animal_reservation_system/core.clj
+++ src/animal_reservation_system/core.clj
@@ -1,6 +1,13 @@
 (ns animal-reservation-system.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def horses #{"All Star"
+              "Genesis"
+              "Pumpkin"
+              "Misty"
+              "Good Morning Sunshine"
+              "Boombird"
+              "Sunny"
+              "Brooke"})
+
+(defn take-random-n [animals n]
+  (take n (shuffle animals)))
--- test/animal_reservation_system/core_test.clj
+++ test/animal_reservation_system/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [animal-reservation-system.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest take-random-n-test
+  (testing "Happy path"
+    (is (= 3 (count (take-random-n horses 3)))))
+
+  (testing "Horses are picked at random"
+    (let [picks (repeatedly 10 #(first (take-random-n horses 1)))]
+      (is (not= 1 (count (frequencies picks)))))))
