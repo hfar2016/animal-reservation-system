5106632d2a9cc664ffb8b1b48b416e41b921db75
Alex Rodioukov <alex@gurudom.com>
2016-05-11 22:41:19 -0600

    Gender specific procedures - a tiny rules engine implemented

--- src/animal_reservation_system/core.clj
+++ src/animal_reservation_system/core.clj
@@ -1,6 +1,8 @@
 (ns animal-reservation-system.core
   (:require [clojure.set :as set]))
 
+(def procedure-rules {"uterine biopsy (mares only)" #(= :mare (:gender %))})
+
 (defn take-random-n [animals n]
   (take n (shuffle animals)))
 
@@ -8,10 +10,17 @@
   (let [overlapping (select-keys day periods)]
     (apply set/union (vals overlapping))))
 
-(defn make-reservation [reservations all-horses {:keys [number date periods] :as request}]
+(defn apply-procedure-rules [animals procedures rules]
+  (if-let [rule-fns (vals (select-keys rules procedures))]
+    (filter (apply every-pred rule-fns) animals)
+    animals))
+
+(defn make-reservation [reservations all-horses {:keys [number date periods procedures] :as request}]
   (let [already-reserved (reserved-for-a-day (get @reservations date) periods)
         available-for-reservation (set/difference all-horses already-reserved)
-        newly-reserved (take-random-n available-for-reservation number)]
+        newly-reserved (-> available-for-reservation
+                           (apply-procedure-rules procedures procedure-rules)
+                           (take-random-n number))]
     (doseq [period periods]
       (swap! reservations update-in [date period] set/union (set newly-reserved)))
     newly-reserved))
--- test/animal_reservation_system/core_test.clj
+++ test/animal_reservation_system/core_test.clj
@@ -8,6 +8,33 @@
 (def gamma {:name "Gamma" :gender :mare})
 (def test-horses #{alpha beta gamma})
 
+(deftest apply-procedure-rules-test
+  (testing "No rules"
+    (is (= test-horses (apply-procedure-rules test-horses ["some procedure"] {}))))
+
+  (testing "No procedures"
+    (is (= test-horses (apply-procedure-rules test-horses nil {"some procedure" (constantly false)}))))
+
+  (testing "Single rule"
+    (is (= [] (apply-procedure-rules
+               test-horses
+               ["exclusive procedure"]
+               {"exclusive procedure" (constantly false)}))))
+
+  (testing "Single procedure and multiple rules"
+    (is (= [gamma] (apply-procedure-rules
+                    test-horses
+                    ["mare-only procedure"]
+                    {"exclusive procedure" (constantly false)
+                     "mare-only procedure" #(= :mare (:gender %))}))))
+
+  (testing "Multiple procedure and multiple rules"
+    (is (= [alpha] (apply-procedure-rules
+                    test-horses
+                    ["procedure-1" "procedure-2" "procedure-3"]
+                    {"procedure-1" #(#{:mare :stallion} (:gender %))
+                     "procedure-3" #(= "Alpha" (:name %))})))))
+
 (deftest take-random-n-test
   (testing "Happy path"
     (is (= 3 (count (take-random-n data/horses 3)))))
@@ -17,29 +44,51 @@
       (is (not= 1 (count (frequencies picks)))))))
 
 (deftest make-reservation-test
-  (testing "Single new reservation"
-    (let [reservations (atom {})
-          request {:number 3 :date "2016-06-01" :periods #{"morning"}}
-          reserved-horses (make-reservation reservations test-horses request)]
-      (is (= test-horses (set reserved-horses)))
-      (is (= {"2016-06-01" {"morning" (set reserved-horses)}} @reservations))))
+  (testing "Blackout rules"
+    (testing "Single new reservation"
+      (let [reservations (atom {})
+            request {:number 3 :date "2016-06-01" :periods #{"morning"}}
+            reserved-horses (make-reservation reservations test-horses request)]
+        (is (= test-horses (set reserved-horses)))
+        (is (= {"2016-06-01" {"morning" (set reserved-horses)}} @reservations))))
 
-  (testing "All are booked"
-    (let [reservations (atom {"2016-06-01" {"morning" #{alpha}
-                                            "afternoon" #{beta}
-                                            "evening" #{gamma}}})
-          request {:number 3 :date "2016-06-01" :periods #{"morning" "afternoon" "evening"}}
-          reserved-horses (make-reservation reservations test-horses request)]
-      (is (= 0 (count reserved-horses)))))
+    (testing "All are booked"
+      (let [reservations (atom {"2016-06-01" {"morning" #{alpha}
+                                              "afternoon" #{beta}
+                                              "evening" #{gamma}}})
+            request {:number 3 :date "2016-06-01" :periods #{"morning" "afternoon" "evening"}}
+            reserved-horses (make-reservation reservations test-horses request)]
+        (is (empty? reserved-horses))))
 
-  (testing "1 slot is available"
-    (let [reservations (atom {"2016-06-01" {"morning" #{alpha}
-                                            "evening" #{gamma}}})
-          request {:number 3 :date "2016-06-01" :periods #{"morning" "afternoon" "evening"}}
-          reserved-horses (make-reservation reservations test-horses request)]
-      (is (= 1 (count reserved-horses)))
-      (is (= beta (first reserved-horses)))
-      (is (= {"2016-06-01" {"morning" #{alpha beta}
-                            "afternoon" #{beta}
-                            "evening" #{beta gamma}}}
-             @reservations)))))
+    (testing "1 slot is available"
+      (let [reservations (atom {"2016-06-01" {"morning" #{alpha}
+                                              "evening" #{gamma}}})
+            request {:number 3 :date "2016-06-01" :periods #{"morning" "afternoon" "evening"}}
+            reserved-horses (make-reservation reservations test-horses request)]
+        (is (= 1 (count reserved-horses)))
+        (is (= beta (first reserved-horses)))
+        (is (= {"2016-06-01" {"morning" #{alpha beta}
+                              "afternoon" #{beta}
+                              "evening" #{beta gamma}}}
+               @reservations)))))
+
+  (testing "Gender specific procedures"
+    (testing "Reservation for physical exam"
+      (let [reservations (atom {})
+            request {:number 3 :date "2016-06-01" :periods #{"morning"} :procedures ["physical exam"]}
+            reserved-horses (make-reservation reservations test-horses request)]
+        (is (= test-horses (set reserved-horses)))
+        (is (= {"2016-06-01" {"morning" (set reserved-horses)}} @reservations))))
+
+    (testing "Reservation for uterine biopsy"
+      (let [reservations (atom {})
+            request {:number 3 :date "2016-06-01" :periods #{"morning"} :procedures ["uterine biopsy (mares only)"]}
+            reserved-horses (make-reservation reservations test-horses request)]
+        (is (= [gamma] reserved-horses))
+        (is (= {"2016-06-01" {"morning" #{gamma}}} @reservations))))
+
+    (testing "Reservation for uterine biopsy when no mares available"
+      (let [reservations (atom {"2016-06-01" {"morning" #{gamma}}})
+            request {:number 3 :date "2016-06-01" :periods #{"morning"} :procedures ["uterine biopsy (mares only)"]}
+            reserved-horses (make-reservation reservations test-horses request)]
+        (is (empty? reserved-horses))))))
